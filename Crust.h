572f20b9bb85652c70bc5306f7d303c668df8485
Matias Savela <msavela@gmail.com>
2012-08-28 19:05:53 +0300

    HTTP authentication, user-agent, imporoved parameter handling etc.

--- Crust.h
+++ Crust.h
@@ -2,45 +2,62 @@
 //  Crust.h
 //
 
-#import <Foundation/Foundation.h>
+#define METHOD_GET @"GET"
+#define METHOD_POST @"POST"
+#define METHOD_PUT @"PUT"
+#define METHOD_DELETE @"DELETE"
+
+typedef enum
+{
+    DEBUG_SILENT = 0,
+    DEBUG_NORMAL = 1,
+    DEBUG_INFO = 2,
+    DEBUG_WARNING = 3,
+    DEBUG_ERROR = 4,
+    DEBUG_CRITICAL = 5
+} DEBUG_LEVEL;
+
+typedef enum
+{
+    GET = 0,
+    POST = 1,
+    PUT = 2,
+    DELETE = 3,
+    CUSTOM = 4
+} REQUEST_METHOD;
 
 @class Crust;
 @protocol CrustDelegate <NSObject>
 @optional
--(void)request:(Crust*)sofa started:(BOOL)flag;
--(void)request:(Crust*)sofa failed:(BOOL)flag;
--(void)request:(Crust*)sofa finished:(BOOL)flag;
+
+-(void)requestStarted:(Crust*)request;
+-(void)requestFailed:(Crust*)request;
+-(void)requestFinished:(Crust*)request;
+-(void)uploadProgress:(Crust*)request progress:(NSInteger)progress totalBytesWritten:(NSInteger)totalBytesWritten totalBytesExpectedToWrite:(NSInteger)totalBytesExpectedToWrite;
 @end
 
 @interface CrustResponse : NSObject
 {
     NSString *text;
     NSMutableData *data;
+    id json;
     NSDictionary *headers;
-    int statusCode;
+    int status;
 }
 
 @property (nonatomic, retain) NSString *text;
 @property (nonatomic, retain) NSMutableData *data;
+@property (nonatomic, retain) id json;
 @property (nonatomic, retain) NSDictionary *headers;
-@property int statusCode;
+@property int status;
+
+-(id)getJSONResponse;
 
 @end
 
 @interface Crust : NSObject <NSURLConnectionDelegate>
 {
-    NSString *method;
-    NSString *path;
-    
-    NSMutableDictionary *parameters;
-    NSMutableDictionary *headers;
-    
-    int timeout;
-    int tag;
-    
-    CrustResponse *response;
-    
-    id <CrustDelegate> delegate;
+    NSURLConnection *connection;
 }
 
 @property (nonatomic, retain) NSString *method;
@@ -48,27 +65,63 @@
 
 @property (nonatomic, retain) NSMutableDictionary *parameters;
 @property (nonatomic, retain) NSMutableDictionary *headers;
+@property (nonatomic, retain) NSMutableDictionary *files;
 
 @property int timeout;
 @property int tag;
 
+@property (assign) BOOL cache;
+
 @property (nonatomic, retain) CrustResponse *response;
 
-@property (nonatomic, assign) id <CrustDelegate> delegate;
+@property (weak) id <CrustDelegate> delegate;
+
+/* Other parameters begin */
+@property (nonatomic, retain) NSString *username;
+@property (nonatomic, retain) NSString *password;
+
+@property (retain) NSString *userAgent;
+
+@property (assign) BOOL permanentAuthentication;
+@property (assign) BOOL activityIndicator;
+/* Other parameters end */
 
 -(void)start;
 -(void)start:(id)aDelegate;
+
+-(void)cancel;
+
+-(void)log:(NSString*)message level:(int)level;
 
 /* Static methods begin */
 +(id)get:(NSString*)aPath parameters:(NSDictionary*)aParams delegate:(id)aDelegate;
 +(id)post:(NSString*)aPath parameters:(NSDictionary*)aParams delegate:(id)aDelegate;
 +(id)put:(NSString*)aPath parameters:(NSDictionary*)aParams delegate:(id)aDelegate;
 +(id)delete:(NSString*)aPath parameters:(NSDictionary*)aParams delegate:(id)aDelegate;
++(id)custom:(NSString*)aMethod path:(NSString*)aPath parameters:(NSDictionary*)aParams delegate:(id)aDelegate;
 
 +(id)get:(NSString*)aPath;
 +(id)post:(NSString*)aPath;
 +(id)put:(NSString*)aPath;
 +(id)delete:(NSString*)aPath;
++(id)custom:(NSString*)aMethod path:(NSString*)aPath;
 /* Static methods end */
+
+/* Utilities begin */
++(NSString*)dataToBase64String:(NSData*)aData;
++(NSString*)pathToBase64String:(NSString*)aPath;
++(NSString*)urlToBase64String:(NSURL*)aUrl;
+/* Utilities end */
+
+@end
+
+@interface Crust (Private)
+
+-(id)initWithMethod:(NSString*)aMethod path:(NSString*)aPath parameters:(NSDictionary*)aParams delegate:(id)aDelegate;
+-(void)setup;
+
+-(NSMutableArray*)requestPool;
+-(void)addToPool;
+-(void)removeFromPool;
 
 @end
--- Crust.m
+++ Crust.m
@@ -4,56 +4,80 @@
 
 #import "Crust.h"
 
+#import "JSONKit.h"
+
+// TODO DEBUG
+#define DEBUG_LEVEL DEBUG_NORMAL
+
 #define DEFAULT_TIMEOUT 120
 
-@interface Crust (Private)
-
--(id)initWithMethod:(NSString*)aMethod path:(NSString*)aPath parameters:(NSDictionary*)aParams delegate:(id)aDelegate;
--(void)setup;
-
--(NSMutableArray*)requestPool;
--(void)addToPool;
--(void)removeFromPool;
-
-@end
-
 @implementation CrustResponse
-@synthesize text, data, headers, statusCode;
+@synthesize text, data, json, headers, status;
+
+-(id)getJSONResponse
+{
+    self.json = [self.data objectFromJSONData];
+    return self.json;
+}
+
 @end
 
 @implementation Crust
-@synthesize method, path, parameters, headers, response, timeout, tag;
-@dynamic delegate;
+@synthesize method, path, parameters, headers, files, response, timeout, tag, cache;
+@synthesize delegate;
+@synthesize username, password;
+@synthesize userAgent;
+@synthesize permanentAuthentication, activityIndicator;
 
 -(void)start
 {
     self.response.data = [NSMutableData data];
-    self.response.statusCode = 0;
+    self.response.json = nil;
+    self.response.status = 0;
     self.response.headers = [[NSDictionary alloc] init];
     
-    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:path]];
+    NSMutableData *body = [NSMutableData data];
+    
+    if([parameters count] > 0)
+    {
+        NSString *parameterString = [[NSString alloc] init];
+        for(NSString *parameter in parameters)
+            parameterString = [parameterString stringByAppendingFormat:@"%@=%@&", parameter, [parameters objectForKey:parameter]];
+        parameterString = [parameterString substringToIndex:[parameterString length] - 1];
+        
+        if(self.method == METHOD_GET)
+            self.path = [self.path stringByAppendingFormat:@"?%@", parameterString];
+        else
+            [body appendData:[parameterString dataUsingEncoding:NSUTF8StringEncoding]];
+    }
+    
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:self.path]];
     [request setHTTPShouldHandleCookies:YES];
     [request setHTTPMethod:method];
+    
+    if(!self.cache) [request setCachePolicy:NSURLRequestReloadIgnoringCacheData];
+    else [request setCachePolicy:NSURLRequestReturnCacheDataElseLoad];
     
     for(NSString *header in headers)
         [request addValue:[headers objectForKey:header] forHTTPHeaderField:header];
     
     request.timeoutInterval = self.timeout;
     
-    NSString *parameterString = [[NSString alloc] init];
-    if([parameters count] > 0)
-    {
-        for(NSString *parameter in parameters)
-            parameterString = [parameterString stringByAppendingFormat:@"%@=%@&", parameter, [parameters objectForKey:parameter]];
-        parameterString = [parameterString substringToIndex:[parameterString length] - 1];
-        [request setHTTPBody:[parameterString dataUsingEncoding:NSASCIIStringEncoding]];
-    }
+    if(self.userAgent)
+        [request addValue:self.userAgent forHTTPHeaderField:@"User-Agent"];
     
-	(void)[[NSURLConnection alloc] initWithRequest:request delegate:self];
+    NSLog(@"BODY: %@", [[NSString alloc] initWithData:body encoding:NSASCIIStringEncoding]);
     
+    [request setHTTPBody:body];
+    [request addValue:[NSString stringWithFormat:@"%d", [body length]] forHTTPHeaderField:@"Content-Length"];
+    
+	connection = [[NSURLConnection alloc] initWithRequest:request delegate:self];
     [self addToPool];
     
-    if(delegate) [delegate request:self started:YES];
+    [self log:@"Request started" level:DEBUG_NORMAL];
+    
+    if(delegate && [delegate respondsToSelector:@selector(requestStarted:)])
+        [delegate requestStarted:self];
 }
 
 -(void)start:(id)aDelegate
@@ -62,48 +86,149 @@
     [self start];
 }
 
+-(void)cancel
+{
+    [self log:@"Cancel request" level:DEBUG_NORMAL];
+    [connection cancel];
+}
+
+-(void)log:(NSString*)message level:(int)level
+{
+    if(level >= DEBUG_LEVEL)
+        NSLog(@"[Crust] %@", message);
+}
+
 - (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)urlResponse
 {
-    NSLog(@"didReceiveResponse");
-    self.response.statusCode = [((NSHTTPURLResponse *)urlResponse) statusCode];
+    [self log:@"didReceiveResponse" level:DEBUG_INFO];
+    self.response.status = [((NSHTTPURLResponse *)urlResponse) statusCode];
     self.response.headers = [((NSHTTPURLResponse *)urlResponse) allHeaderFields];
     [self.response.data setLength:0];
 }
 
 - (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data
 {
-    NSLog(@"didReceiveData");
+    [self log:@"didReceiveData" level:DEBUG_INFO];
     [self.response.data appendData:data];
 }
 
 - (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error
 {
-    NSLog(@"didFailWithError: %@", [error description]);
+    [self log:[NSString stringWithFormat:@"didFailWithError: %@", [error description]] level:DEBUG_ERROR];
     [self removeFromPool];
-    if(delegate) [delegate request:self failed:YES];
+    
+    [self log:@"Request failed" level:DEBUG_ERROR];
+    
+    if(delegate && [delegate respondsToSelector:@selector(requestFailed:)])
+        [delegate requestFailed:self];
 }
 
 - (void)connectionDidFinishLoading:(NSURLConnection *)connection
 {
-    NSLog(@"connectionDidFinishLoading");
+    [self log:@"connectionDidFinishLoading" level:DEBUG_INFO];
     self.response.text = [[NSString alloc] initWithData:self.response.data encoding:NSASCIIStringEncoding];
-    NSLog(@"Response: %@", self.response.text);
+    self.response.json = [self.response.data objectFromJSONData];
+    [self log:[NSString stringWithFormat:@"Response: %@", self.response.text] level:DEBUG_INFO];
     
     [self removeFromPool];
-    if(delegate) [delegate request:self finished:YES];
+    if(delegate && [delegate respondsToSelector:@selector(requestFinished:)])
+        [delegate requestFinished:self];
+}
+
+- (void)connection:(NSURLConnection *)connection didSendBodyData:(NSInteger)bytesWritten totalBytesWritten:(NSInteger)totalBytesWritten totalBytesExpectedToWrite:(NSInteger)totalBytesExpectedToWrite
+{
+    [self log:@"didSendBodyData" level:DEBUG_INFO];
+    if(delegate && [delegate respondsToSelector:@selector(uploadProgress:progress:totalBytesWritten:totalBytesExpectedToWrite:)])
+    {
+        NSInteger progress = [[NSNumber numberWithFloat:((float)totalBytesWritten / (float)totalBytesExpectedToWrite) * 100.0f] integerValue];
+        [delegate uploadProgress:self progress:progress totalBytesWritten:totalBytesWritten totalBytesExpectedToWrite:totalBytesExpectedToWrite];
+    }
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveAuthenticationChallenge:(NSURLAuthenticationChallenge *)challenge
+{
+    if([challenge previousFailureCount] == 0 && self.username && self.password)
+    {
+        NSURLCredentialPersistence persistence;
+        if(self.permanentAuthentication) persistence = NSURLCredentialPersistencePermanent;
+        else persistence = NSURLCredentialPersistenceForSession;
+        
+        [self log:@"Received authentication challenge" level:DEBUG_NORMAL];
+        NSURLCredential *credentials = [NSURLCredential credentialWithUser:self.username
+                                                                  password:self.password
+                                                               persistence:persistence];        
+        [[challenge sender] useCredential:credentials forAuthenticationChallenge:challenge];  
+    }
+    else
+        [self log:@"Authentication failure" level:DEBUG_ERROR];
+}
+
+- (BOOL)connectionShouldUseCredentialStorage:(NSURLConnection *)connection
+{
+    return YES;
 }
 
 /* Static methods begin */
-+(id)get:(NSString*)aPath parameters:(NSDictionary*)aParams delegate:(id)aDelegate { return [[Crust alloc] initWithMethod:@"GET" path:aPath parameters:aParams delegate:aDelegate]; }
-+(id)post:(NSString*)aPath parameters:(NSDictionary*)aParams delegate:(id)aDelegate { return [[Crust alloc] initWithMethod:@"POST" path:aPath parameters:aParams delegate:aDelegate]; }
-+(id)put:(NSString*)aPath parameters:(NSDictionary*)aParams delegate:(id)aDelegate { return [[Crust alloc] initWithMethod:@"PUT" path:aPath parameters:aParams delegate:aDelegate]; }
-+(id)delete:(NSString*)aPath parameters:(NSDictionary*)aParams delegate:(id)aDelegate { return [[Crust alloc] initWithMethod:@"DELETE" path:aPath parameters:aParams delegate:aDelegate]; }
++(id)get:(NSString*)aPath parameters:(NSDictionary*)aParams delegate:(id)aDelegate { return [[Crust alloc] initWithMethod:METHOD_GET path:aPath parameters:aParams delegate:aDelegate]; }
++(id)post:(NSString*)aPath parameters:(NSDictionary*)aParams delegate:(id)aDelegate { return [[Crust alloc] initWithMethod:METHOD_POST path:aPath parameters:aParams delegate:aDelegate]; }
++(id)put:(NSString*)aPath parameters:(NSDictionary*)aParams delegate:(id)aDelegate { return [[Crust alloc] initWithMethod:METHOD_PUT path:aPath parameters:aParams delegate:aDelegate]; }
++(id)delete:(NSString*)aPath parameters:(NSDictionary*)aParams delegate:(id)aDelegate { return [[Crust alloc] initWithMethod:METHOD_DELETE path:aPath parameters:aParams delegate:aDelegate]; }
++(id)custom:(NSString*)aMethod path:(NSString*)aPath parameters:(NSDictionary*)aParams delegate:(id)aDelegate
+{
+    return [[Crust alloc] initWithMethod:aMethod path:aPath parameters:aParams delegate:aDelegate];
+}
 
-+(id)get:(NSString*)aPath { return [[Crust alloc] initWithMethod:@"GET" path:aPath parameters:nil delegate:nil]; }
-+(id)post:(NSString*)aPath { return [[Crust alloc] initWithMethod:@"POST" path:aPath parameters:nil delegate:nil]; }
-+(id)put:(NSString*)aPath { return [[Crust alloc] initWithMethod:@"PUT" path:aPath parameters:nil delegate:nil]; }
-+(id)delete:(NSString*)aPath { return [[Crust alloc] initWithMethod:@"DELETE" path:aPath parameters:nil delegate:nil]; }
++(id)get:(NSString*)aPath { return [[Crust alloc] initWithMethod:METHOD_GET path:aPath parameters:nil delegate:nil]; }
++(id)post:(NSString*)aPath { return [[Crust alloc] initWithMethod:METHOD_POST path:aPath parameters:nil delegate:nil]; }
++(id)put:(NSString*)aPath { return [[Crust alloc] initWithMethod:METHOD_PUT path:aPath parameters:nil delegate:nil]; }
++(id)delete:(NSString*)aPath { return [[Crust alloc] initWithMethod:METHOD_DELETE path:aPath parameters:nil delegate:nil]; }
++(id)custom:(NSString*)aMethod path:(NSString*)aPath { return [[Crust alloc] initWithMethod:aMethod path:aPath parameters:nil delegate:nil]; }
 /* Static methods end */
+
+/* Utilities begin */
++(NSString*)dataToBase64String:(NSData*)aData
+{
+    const uint8_t* input = (const uint8_t*)[aData bytes];
+    NSInteger length = [aData length];
+    
+    static char table[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+    
+    NSMutableData* data = [NSMutableData dataWithLength:((length + 2) / 3) * 4];
+    uint8_t* output = (uint8_t*)data.mutableBytes;
+    
+    NSInteger i;
+    for(i=0; i < length; i += 3)
+    {
+        NSInteger value = 0;
+        NSInteger j;
+        for(j = i; j < (i + 3); j++)
+        {
+            value <<= 8;
+            
+            if(j < length)
+                value |= (0xFF & input[j]);
+        }
+        
+        NSInteger theIndex = (i / 3) * 4;
+        output[theIndex + 0] =                    table[(value >> 18) & 0x3F];
+        output[theIndex + 1] =                    table[(value >> 12) & 0x3F];
+        output[theIndex + 2] = (i + 1) < length ? table[(value >> 6)  & 0x3F] : '=';
+        output[theIndex + 3] = (i + 2) < length ? table[(value >> 0)  & 0x3F] : '=';
+    }
+    
+    return [[NSString alloc] initWithData:data encoding:NSASCIIStringEncoding];
+}
+
++(NSString*)pathToBase64String:(NSString*)aPath
+{
+    return [Crust dataToBase64String:[NSData dataWithContentsOfFile:aPath]];
+}
+
++(NSString*)urlToBase64String:(NSURL*)aUrl
+{
+     return [Crust dataToBase64String:[NSData dataWithContentsOfURL:aUrl]];
+}
+/* Utilities end */
 
 @end
 
@@ -124,10 +249,11 @@
 
 -(void)setup
 {
-    self.method = @"GET";
+    self.method = METHOD_GET;
     self.headers = [[NSMutableDictionary alloc] init];
     self.response = [[CrustResponse alloc] init];
     self.timeout = DEFAULT_TIMEOUT;
+    self.activityIndicator = YES;
 }
 
 -(NSMutableArray*)requestPool
@@ -141,7 +267,8 @@
 -(void)addToPool
 {
     [[self requestPool] addObject:self];
-    [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:YES];
+    if(self.activityIndicator)
+        [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:YES];
 }
 
 -(void)removeFromPool
